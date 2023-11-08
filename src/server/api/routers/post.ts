ba289fade312d372292562ab6029492d0ae3cc4d
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-11-08 21:59:57 +0800

    bug fix

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,32 +1,24 @@
 import { z } from "zod";
+import { User } from "@clerk/nextjs/dist/types/server";
+import { clerkClient } from "@clerk/nextjs";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.imageUrl,
+  };
+};
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+  testingToGetAllUser: publicProcedure.query(async ({ input }) => {
+    const users = (
+      await clerkClient.users.getUserList({
+        limit: 100,
+      })
+    ).map(filterUserForClient);
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
+    return users;
   }),
 });
