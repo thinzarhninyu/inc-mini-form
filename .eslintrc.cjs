79e0786bbd8df507c49be9a803dc7e0dbd24e3b4
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-11-17 16:04:47 +0800

    eslint fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,6 +33,7 @@ const config = {
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-argument": "off"
   },
 };
 
